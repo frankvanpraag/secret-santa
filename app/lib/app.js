c2f48a75cae8bc7291d37cfffc705631e3342039
frankvanpraag <16331016+frankvanpraag@users.noreply.github.com>
2020-11-01 15:26:14 +1100

    Update app.js

--- app/lib/app.js
+++ app/lib/app.js
@@ -25,7 +25,7 @@ class App {
     return JSON.parse(db.toString());
   }
 
-  function shuffle(sourceArray) {
+  shuffle(sourceArray) {
     for (var i = 0; i < sourceArray.length - 1; i++) {
         var j = i + Math.floor(Math.random() * (sourceArray.length - i));
         var temp = sourceArray[j];

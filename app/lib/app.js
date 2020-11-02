6cef73d833f9fc6667754bbed9f7df2a6f5cb331
frankvanpraag <16331016+frankvanpraag@users.noreply.github.com>
2020-11-02 14:33:16 +1100

    Update app.js

--- app/lib/app.js
+++ app/lib/app.js
@@ -2,15 +2,12 @@ const fs = require('fs');
 const ejs = require('ejs');
 const SendGridAdapter = require('../adapters/send-grid');
 const pool = "POOL_2sNvzmrYrdn9RQ1";
-const mailingList = "CG_8tVp5T22DHGyRtH";
+const mailingList = "CG_eKce12cVmjCadxj";
 const hostname = "syd1.qualtrics.com";
 // const hostname = "2aee86ecb4940555cf2afa068d2ba5a8.m.pipedream.net";
 const getMailingListContactsQuery = "/API/v3/directories/" + pool + "/mailinglists/" + mailingList + "/contacts";
 const getMailingListContactsUrl = "https://" + hostname + getMailingListContactsQuery;
 var request = require('request');
-var personList = {},
-	totalResults = 0,
-	resultsDownloaded = 0;
 
 class App {
   constructor () {
@@ -28,70 +25,7 @@ class App {
     return JSON.parse(db.toString());
   }
 
-  function httpRequest(params, postData) {
-      return new Promise(function(resolve, reject) {
-          var req = http.request(params, function(res) {
-              // on bad status, reject
-              // on response data, cumulate it
-              // on end, parse and resolve
-          });
-          // on request error, reject
-          // if there's post data, write it to the request
-          // important: end the request req.end()
-      });
-  }
-
-
-  getContacts(brand, key, api, surveyId) {
-    // Helper fuctions
-    function sleep(milliseconds) { // Yes, terrible
-      const date = Date.now();
-      let currentDate = null;
-      do {
-        currentDate = Date.now();
-      } while (currentDate - date < milliseconds);
-    }
-    
-    function shuffle(sourceArray) {
-      for (var i = 0; i < sourceArray.length - 1; i++) {
-          var j = i + Math.floor(Math.random() * (sourceArray.length - i));
-          var temp = sourceArray[j];
-          sourceArray[j] = sourceArray[i];
-          sourceArray[i] = temp;
-      }
-      return sourceArray;
-    }
-
-    // write buddy name and id to XMD
-    // Use Qualtrics API to get all SE members (max 200)
-    var options = {
-      method: 'GET',
-      headers: { 'accept': '*/*', 'X-API-TOKEN': key},
-      url: getMailingListContactsUrl,
-      qs: {pageSize: '200'}
-    };
-
-    // Get all contacts in SE buddy list
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-      console.log(body);
-      personList = JSON.parse(body).result.elements;
-    	totalResults++;
-	    resultsDownloaded++;
-      //console.log("PERSONLIST: " + JSON.stringify(personList, undefined, 2)); // {"result":{"elements":[{"contactId":"CID_3pIMBkMDJUt5qWp","firstName":"Eeee","lastName":"Egbert","email":"q5@vanpraag.com","phone":null,"extRef":"q5@vanpraag.com","language":null,"unsubscribed":false},{"contactId":"CID_2rypp6zL9UdbiLj","firstName":"Aaaa","lastName":"Aardvaark","email":"q1@vanpraag.com","phone":null,"extRef":"q1@vanpraag.com","language":null,"unsubscribed":false},{"contactId":"CID_aggZq9ziA7j6iiN","firstName":"Bbbb","lastName":"Bullwark","email":"q2@vanpraag.com","phone":null,"extRef":"q2@vanpraag.com","language":null,"unsubscribed":false},{"contactId":"CID_81VryhahElQBxXL","firstName":"Dddd","lastName":"Dopermine","email":"q4@vanpraag.com","phone":null,"extRef":"q4@vanpraag.com","language":null,"unsubscribed":false},{"contactId":"CID_2mWrnio45kZYTTn","firstName":"Cccc","lastName":"Chipotle","email":"q3@vanpraag.com","phone":null,"extRef":"q3@vanpraag.com","language":null,"unsubscribed":false}]
-      //console.log("personList: " + JSON.stringify(personList, undefined, 2));
-      //console.log("RESULT[1]: " + JSON.stringify(personList[1], undefined, 2));
-      console.log("RESULT[1].firstName: " + JSON.stringify(personList[1].firstName, undefined, 2));
-    });
-    console.log("sleeping for a while...");
-    sleep(3000);
-    console.log("sleeping done");
-    return personList;
-  }
-
-  // ----------------------------------
-	
-  mixItUpWorkingButCrap (brand, key, api, surveyId) {
+  mixItUp (brand, key, api, surveyId) {
     function shuffle(sourceArray) {
       for (var i = 0; i < sourceArray.length - 1; i++) {
           var j = i + Math.floor(Math.random() * (sourceArray.length - i));
@@ -121,26 +55,7 @@ class App {
       //console.log("RESULT[1]: " + JSON.stringify(personList[1], undefined, 2));
       //console.log("RESULT[1].firstName: " + JSON.stringify(personList[1].firstName, undefined, 2));
       
-function resolveAfter2Seconds() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('resolved');
-    }, 2000);
-  });
-}
-
-async function asyncCall() {
-  console.log('calling');
-  const result = await resolveAfter2Seconds();
-  console.log(result);
-  // expected output: "resolved"
-}
-
-//asyncCall()
-      
       var contacts = [];
-
-Promise.all([      
       personList.forEach((person,index)=>{
         // Retrieve previous matches to avoid
         var options2 = {
@@ -154,38 +69,18 @@ Promise.all([
           let previousMatches = JSON.parse(body).result.embeddedData.previousMatches;
           // Construct short list of contacts
           var contact = { contactId:person.contactId, extRef:person.extRef, previousMatches:previousMatches };
-return Promise.all(contacts.map(function (contact) {
-  return contact.json();
-}));
           console.log("CONTACT: " + JSON.stringify(contact, undefined, 2));
-     //     contacts.push(contact);
-          //console.log("CONTACTS: " + JSON.stringify(contacts, undefined, 2));
-          //console.log("---");
-          //console.log("Shuffled CONTACTS: " + JSON.stringify(shuffle(contacts), undefined, 2));
-          //console.log("---");
+          contacts.push(contact);
+          console.log("CONTACTS: " + JSON.stringify(contacts, undefined, 2));
+          console.log("---");
+          console.log("Shuffled CONTACTS: " + JSON.stringify(shuffle(contacts), undefined, 2));
+          console.log("---");
         });
-      })
-]).then(function (data) {
-	// Log the data to the console
-	// You would do something with both sets of data here
-  	console.log("CONTACTS FINAL: " + JSON.stringify(contacts, undefined, 2)); // XXX WHY IS THIS PRINTED FRIST (and hence EMPTY)!?
-	console.log(data);
-}).catch(function (error) {
-	// if there's an error, log it
-	console.log(error);
-});
-
-      
-      
+        //console.log("CONTACTS FINAL: " + JSON.stringify(contacts, undefined, 2)); // XXX WHY IS THIS PRINTED FRIST (and hence EMPTY)!?
+      });
     });
   }
 
-	
-	
-	
-	
-  // -----------------------
-	
   addSubscriber (data) {
     const storage = this.getStorage();
     storage.subscribers.push(data);
@@ -337,5 +232,3 @@ return Promise.all(contacts.map(function (contact) {
     return array;
   }
 }
-
-module.exports = new App();

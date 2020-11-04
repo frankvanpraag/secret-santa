0e42344012babd293517451b3bd33939dd0d9fb1
frankvanpraag <16331016+frankvanpraag@users.noreply.github.com>
2020-11-04 21:01:50 +1100

    Update app.js

--- app/lib/app.js
+++ app/lib/app.js
@@ -32,26 +32,10 @@ class App {
       }
       return sourceArray;
     }
-    // write buddy name and id to XMD
-    // Use Qualtrics API to get all SE members (max 200)
-    var options = {
-      method: 'GET',
-      headers: { 'accept': '*/*', 'X-API-TOKEN': key},
-      url: getMailingListContactsUrl,
-      qs: {pageSize: '200'}
-    };
-    // Get all contacts in SE buddy list
-    request(options, function (error, response, body) {
-      if (error) throw new Error(error);
-      console.log(body);
-      let personList = JSON.parse(body).result.elements;
-      /*console.log("PERSONLIST: " + JSON.stringify(personList, undefined, 2)); // {"result":{"elements":[{"contactId":"CID_3pIMBkMDJUt5qWp","firstName":"Eeee","lastName":"Egbert","email":"q5@vanpraag.com","phone":null,"extRef":"q5@vanpraag.com","language":null,"unsubscribed":false},{"contactId":"CID_2rypp6zL9UdbiLj","firstName":"Aaaa","lastName":"Aardvaark","email":"q1@vanpraag.com","phone":null,"extRef":"q1@vanpraag.com","language":null,"unsubscribed":false},{"contactId":"CID_aggZq9ziA7j6iiN","firstName":"Bbbb","lastName":"Bullwark","email":"q2@vanpraag.com","phone":null,"extRef":"q2@vanpraag.com","language":null,"unsubscribed":false},{"contactId":"CID_81VryhahElQBxXL","firstName":"Dddd","lastName":"Dopermine","email":"q4@vanpraag.com","phone":null,"extRef":"q4@vanpraag.com","language":null,"unsubscribed":false},{"contactId":"CID_2mWrnio45kZYTTn","firstName":"Cccc","lastName":"Chipotle","email":"q3@vanpraag.com","phone":null,"extRef":"q3@vanpraag.com","language":null,"unsubscribed":false}]
-      console.log("RESULT: " + JSON.stringify(personList, undefined, 2));
-      console.log("RESULT[1]: " + JSON.stringify(personList[1], undefined, 2));
-      console.log("RESULT[1].firstName: " + JSON.stringify(personList[1].firstName, undefined, 2));*/
-      processArray(personList, key);
-      console.log("CONTACTS FINAL XXX: " + JSON.stringify(contacts, undefined, 2));
-    });
+    
+    justDoIt(key);
+    
+    console.log("CONTACTS FINAL XXX: " + JSON.stringify(contacts, undefined, 2));
   }
   addSubscriber (data) {
     const storage = this.getStorage();
@@ -208,6 +192,28 @@ async function processItem(person, key) {
   catch(error) {
   }
 }
+
+async function justDoIt(key) {
+  // write buddy name and id to XMD
+  // Use Qualtrics API to get all SE members (max 200)
+  var options = {
+    method: 'GET',
+    headers: { 'accept': '*/*', 'X-API-TOKEN': key},
+    url: getMailingListContactsUrl,
+    qs: {pageSize: '200'}
+  };
+  // Get all contacts in SE buddy list
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+    console.log(body);
+    let personList = JSON.parse(body).result.elements;
+    /*console.log("PERSONLIST: " + JSON.stringify(personList, undefined, 2)); // {"result":{"elements":[{"contactId":"CID_3pIMBkMDJUt5qWp","firstName":"Eeee","lastName":"Egbert","email":"q5@vanpraag.com","phone":null,"extRef":"q5@vanpraag.com","language":null,"unsubscribed":false},{"contactId":"CID_2rypp6zL9UdbiLj","firstName":"Aaaa","lastName":"Aardvaark","email":"q1@vanpraag.com","phone":null,"extRef":"q1@vanpraag.com","language":null,"unsubscribed":false},{"contactId":"CID_aggZq9ziA7j6iiN","firstName":"Bbbb","lastName":"Bullwark","email":"q2@vanpraag.com","phone":null,"extRef":"q2@vanpraag.com","language":null,"unsubscribed":false},{"contactId":"CID_81VryhahElQBxXL","firstName":"Dddd","lastName":"Dopermine","email":"q4@vanpraag.com","phone":null,"extRef":"q4@vanpraag.com","language":null,"unsubscribed":false},{"contactId":"CID_2mWrnio45kZYTTn","firstName":"Cccc","lastName":"Chipotle","email":"q3@vanpraag.com","phone":null,"extRef":"q3@vanpraag.com","language":null,"unsubscribed":false}]
+    console.log("RESULT: " + JSON.stringify(personList, undefined, 2));
+    console.log("RESULT[1]: " + JSON.stringify(personList[1], undefined, 2));
+    console.log("RESULT[1].firstName: " + JSON.stringify(personList[1].firstName, undefined, 2));*/
+    processArray(personList, key);
+  });
+
 async function processArray(personList, key) {
   for (const person of personList) {
     await processItem(person, key);
